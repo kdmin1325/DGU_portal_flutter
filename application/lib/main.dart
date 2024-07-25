cb355a6df0ef3b024635922640b11369b022c188
lce0517 <ashaeun2010@gmail.com>
2024-07-25 17:01:05 +0900

    [update] 웹뷰 로딩 오류 해결 및 일부 웹뷰 헤더 설정, 기기 상단바 기본 설정 유지

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 void main() {
   runApp(const MyApp());
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.white,
     statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
   ));
 }
 
@@ -26,86 +26,89 @@ class MyApp extends StatelessWidget {
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-        statusBarIconBrightness: Brightness.dark,
-      ),
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
-          child: AppBar(
-            backgroundColor: Color(0xFFFFA500),
-            toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-            title: Center(
-              child: Image.asset('assets/dgumain.png', height: MediaQuery.of(context).size.height * 0.05),
-            ),
-          ),
-        ),
-        body: LayoutBuilder(
-          builder: (context, constraints) {
-            double squareIconSizeFactor = 0.4;
-            double circleIconSizeFactor = 0.3;
-            double squareIconSize = constraints.maxWidth * squareIconSizeFactor;
-            double circleIconSize = constraints.maxWidth * circleIconSizeFactor;
-            double spacing = constraints.maxWidth * 0.04;
-
-            return Padding(
-              padding: EdgeInsets.all(spacing),
-              child: ListView(
-                children: [
-                  GridView.count(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: spacing,
-                    mainAxisSpacing: spacing,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      _buildGridItem(context, 'assets/eclass.png', squareIconSize, 'https://eclass.dongguk.ac.kr/home/mainHome/Form/main'),
-                      _buildGridItem(context, 'assets/noti.png', squareIconSize, 'https://web.dongguk.ac.kr/article/generalnotice/list'),
-                      _buildGridItem(context, 'assets/bus.png', squareIconSize, 'https://dongguk.unibus.kr/#/'),
-                      _buildGridItem(context, 'assets/scnoti.png', squareIconSize, 'https://web.dongguk.ac.kr/article/acdnotice/list'),
-                    ],
-                  ),
-                  SizedBox(height: spacing),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      _buildCircleItem(context, 'assets/mnoti.png', circleIconSize, 'https://web.dongguk.ac.kr/article/servicenotice/list'),
-                      _buildCircleItem(context, 'assets/donoti.png', circleIconSize, 'https://dorm.dongguk.ac.kr/'),
-                      _buildCircleItem(context, 'assets/uni.png', circleIconSize, null), // 단과대 아이콘은 클릭 기능 없음
-                    ],
-                  ),
-                ],
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            PreferredSize(
+              preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
+              child: AppBar(
+                backgroundColor: Color(0xFFF89805),
+                toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+                title: Center(
+                  child: Image.asset('assets/dgumain.png', height: MediaQuery.of(context).size.height * 0.06),
+                ),
+                automaticallyImplyLeading: false,
               ),
-            );
-          },
+            ),
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double squareIconSizeFactor = 0.4;
+                  double circleIconSizeFactor = 0.3;
+                  double squareIconSize = constraints.maxWidth * squareIconSizeFactor;
+                  double circleIconSize = constraints.maxWidth * circleIconSizeFactor;
+                  double spacing = constraints.maxWidth * 0.04;
+
+                  return Padding(
+                    padding: EdgeInsets.all(spacing),
+                    child: ListView(
+                      children: [
+                        GridView.count(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: spacing,
+                          mainAxisSpacing: spacing,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          children: [
+                            _buildGridItem(context, 'assets/eclass.png', squareIconSize, 'https://eclass.dongguk.ac.kr/home/mainHome/Form/main'),
+                            _buildGridItem(context, 'assets/noti.png', squareIconSize, 'https://web.dongguk.ac.kr/article/generalnotice/list', true),
+                            _buildGridItem(context, 'assets/bus.png', squareIconSize, 'https://dongguk.unibus.kr/#/'),
+                            _buildGridItem(context, 'assets/scnoti.png', squareIconSize, 'https://web.dongguk.ac.kr/article/acdnotice/list', true),
+                          ],
+                        ),
+                        SizedBox(height: spacing),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _buildCircleItem(context, 'assets/mnoti.png', circleIconSize, 'https://web.dongguk.ac.kr/article/servicenotice/list', true),
+                            _buildCircleItem(context, 'assets/donoti.png', circleIconSize, 'https://dorm.dongguk.ac.kr/'),
+                            _buildCircleItem(context, 'assets/uni.png', circleIconSize, null), // 단과대 아이콘은 클릭 기능 없음
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _buildGridItem(BuildContext context, String assetPath, double iconSize, String? url) {
+  Widget _buildGridItem(BuildContext context, String assetPath, double iconSize, String? url, [bool showHeader = false]) {
     return GestureDetector(
-      onTap: () => _openWebView(context, url),
+      onTap: () => _openWebView(context, url, showHeader),
       child: Center(
         child: Image.asset(assetPath, width: iconSize, height: iconSize),
       ),
     );
   }
 
-  Widget _buildCircleItem(BuildContext context, String assetPath, double iconSize, String? url) {
+  Widget _buildCircleItem(BuildContext context, String assetPath, double iconSize, String? url, [bool showHeader = false]) {
     return GestureDetector(
-      onTap: () => _openWebView(context, url),
+      onTap: () => _openWebView(context, url, showHeader),
       child: Image.asset(assetPath, width: iconSize, height: iconSize),
     );
   }
 
-  void _openWebView(BuildContext context, String? url) {
+  void _openWebView(BuildContext context, String? url, bool showHeader) {
     if (url != null) {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => WebViewScreen(url: url)),
+        MaterialPageRoute(builder: (context) => WebViewScreen(url: url, showHeader: showHeader)),
       );
     } else {
       print('URL is null');
@@ -115,29 +118,81 @@ class MainScreen extends StatelessWidget {
 
 class WebViewScreen extends StatefulWidget {
   final String url;
+  final bool showHeader;
 
-  WebViewScreen({required this.url});
+  WebViewScreen({required this.url, required this.showHeader});
 
   @override
   _WebViewScreenState createState() => _WebViewScreenState();
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
-  late final WebViewController _controller;
+  late WebViewController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..loadRequest(Uri.parse(widget.url));
+    _controller = WebViewController();
+    _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    _controller.setNavigationDelegate(
+      NavigationDelegate(
+        onPageStarted: (String url) {
+          setState(() {
+            _isLoading = true;
+          });
+        },
+        onPageFinished: (String url) {
+          setState(() {
+            _isLoading = false;
+          });
+        },
+        onWebResourceError: (WebResourceError error) {
+          print('Page resource error: ${error.description}');
+        },
+      ),
+    );
+    _controller.loadRequest(Uri.parse(widget.url));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: WebViewWidget(
-          controller: _controller,
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          _controller.goBack();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              if (widget.showHeader)
+                PreferredSize(
+                  preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
+                  child: AppBar(
+                    backgroundColor: Color(0xFFF89805),
+                    toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+                    title: Center(
+                      child: Image.asset('assets/dgumain.png', height: MediaQuery.of(context).size.height * 0.06),
+                    ),
+                    automaticallyImplyLeading: false,
+                  ),
+                ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    WebViewWidget(
+                      controller: _controller,
+                    ),
+                    if (_isLoading) Center(child: CircularProgressIndicator()),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
