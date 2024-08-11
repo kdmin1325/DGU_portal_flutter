59bb8d617dd2694c4a3dbecba0f9061ad331779b
min <dongmin@dongguk.ac.kr>
2024-08-12 02:16:38 +0900

    [update] 어노테이션 공백 제거

--- app_server/application.py
+++ app_server/application.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import crawling
+
 application = Flask(__name__)
 
-@application .route('/')
+@application.route('/')
 def Check_connection():
     return '1'
 
-@application .route('/num')
+@application.route('/num')
 def num():
     return '10'
 
+@application.route('/information')
+def information():
+    return jsonify(crawling.noti_page_parser('https://wise.dongguk.ac.kr/article/generalnotice/list#none'))
+
 if __name__ == '__main__':
-    application .run()
+    application.run()
