0b694012a32479666b0d2c00b37f505e63992d5a
min <dongmin@dongguk.ac.kr>
2024-08-15 18:07:42 +0900

    [update] 크롤링 파일 업데이트

--- app_server/crawling.py
+++ app_server/crawling.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-def noti_page_parser(link):
+def noti_page_parser(noti_link, code_link):
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(link, headers=headers)
+    data = requests.get(noti_link, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
 
     #리턴용 리스트 및 변수 작성
@@ -11,7 +11,6 @@ def noti_page_parser(link):
 
     for i in range(1, 10):
         Noti_num_list = list()
-        i = i + 1
 
         # 공지 제목
         title = soup.select_one(
@@ -20,7 +19,7 @@ def noti_page_parser(link):
 
         # 고유 코드
         title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.mark > span' % i)
-        Noti_num_list.append(title.get_text(strip=True))
+        Noti_num_list.append(code_link + title.get_text(strip=True))
 
         if (title.get_text(strip=True) == "공지"):
             continue
@@ -28,10 +27,7 @@ def noti_page_parser(link):
         #공지 날짜
         title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.top > div.info > span:nth-child(1)' %i)
         Noti_num_list.append(title.get_text(strip=True))
-        i = i + 1
 
         Noti_Compression.append(Noti_num_list)
 
     return Noti_Compression
-
-print(noti_page_parser('https://wise.dongguk.ac.kr/article/generalnotice/list#none'))
