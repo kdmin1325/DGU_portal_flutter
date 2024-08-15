e230fd0a391d5297a1701c09566ca58b7b2446cd
min <dongmin@dongguk.ac.kr>
2024-08-15 18:05:09 +0900

    [update] 페이지 링크 모음 라이브러리

---
+++ app_server/url.py
@@ -0,0 +1,7 @@
+#일반 공지 페이지
+general = 'https://wise.dongguk.ac.kr/article/generalnotice/list#none'
+
+acd = 'https://wise.dongguk.ac.kr/article/acdnotice/list'
+
+empprg = 'https://wise.dongguk.ac.kr/article/empprg/list'
+
