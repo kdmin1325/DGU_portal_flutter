9274f5a6aa4040e81576211c31bc3966bfcd7db4
lce0517 <ashaeun2010@gmail.com>
2024-08-23 17:21:03 +0900

    [update] url update(/제거)

--- application/lib/utils/urls.dart
+++ application/lib/utils/urls.dart
@@ -1,5 +1,5 @@
 class ApiUrls {
-  static const String SERVERIP = 'http://a9b8c7d6e5f4g3h2i1j0klmnopqrst.ap-northeast-2.elasticbeanstalk.com/';
+  static const String SERVERIP = 'http://a9b8c7d6e5f4g3h2i1j0klmnopqrst.ap-northeast-2.elasticbeanstalk.com';
 }
 
 class MainUrls {
