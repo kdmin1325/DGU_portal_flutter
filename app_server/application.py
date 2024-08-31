ccd062dfb5eecb3c3f842edf4528a0db8ec08172
min <dongmin@dongguk.ac.kr>
2024-09-01 01:14:22 +0900

    [update] 딕셔너리 방식의 저장 추가 및 추출 요소 변경

--- app_server/application.py
+++ app_server/application.py
@@ -1,67 +1,74 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # CORS 추가
 import crawling
 import time as t
 import url
-import random
 
-#업데이트 주기 파악용 시간 측정
+# 업데이트 주기 파악용 시간 측정
 start_general_noti_time = t.time()
 start_acd_noti_time = t.time()
 start_empprg_noti_time = t.time()
 
-#각 공지 크롤링 함수
+# 각 공지 크롤링 함수
 general_noti = crawling.noti_page_parser(url.general)
 acd_noti = crawling.noti_page_parser(url.acd)
 empprg_noti = crawling.noti_page_parser(url.empprg)
-application = Flask(__name__)
 
-#연결 확인용
+application = Flask(__name__)
+CORS(application)  # Flask 앱에 CORS 적용
+
+
+# 연결 확인용
 @application.route('/')
 def Check_connection():
     return '1'
 
-#호출할 공지 갯수 설정
-@application.route('/num')
+
+# 호출할 공지 갯수 설정
+@application.route('/num', methods=['GET'])
 def num():
     return '10'
 
-#일반 공지
-@application.route('/information/generalnotice')
+
+# 일반 공지
+@application.route('/information/generalnotice', methods=['GET'])
 def generalnotice_information():
-    global start_general_noti_time, general_noti, acd_noti, empprg_noti
+    global start_general_noti_time, general_noti
     now_time = t.time() - start_general_noti_time
     print("일반 공지 요청")
 
     if(now_time > 3600):
-        start_time = t.time()
+        start_general_noti_time = t.time()
         print("공지 새로 고침")
         general_noti = crawling.noti_page_parser(url.general)
 
     return jsonify(general_noti)
 
-#학사 공지
+
+# 학사 공지
 @application.route('/information/acdnotice')
 def acdnotice_information():
-    global start_acd_noti_time, general_noti, acd_noti, empprg_noti
+    global start_acd_noti_time, acd_noti
     now_time = t.time() - start_acd_noti_time
     print("학사 공지 요청")
 
     if(now_time > 3600):
-        start_time = t.time()
+        start_acd_noti_time = t.time()
         print("공지 새로 고침")
         acd_noti = crawling.noti_page_parser(url.acd)
 
     return jsonify(acd_noti)
 
-#취업 공지
+
+# 취업 공지
 @application.route('/information/empprgnoti')
 def empprgnoti_information():
-    global start_empprg_noti_time, general_noti, acd_noti, empprg_noti
+    global start_empprg_noti_time, empprg_noti
     now_time = t.time() - start_empprg_noti_time
     print("취업 공지 요청")
 
     if(now_time > 3600):
-        start_time = t.time()
+        start_empprg_noti_time = t.time()
         print("공지 새로 고침")
         empprg_noti = crawling.noti_page_parser(url.empprg)
 
--- app_server/crawling.py
+++ app_server/crawling.py
@@ -1,33 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
+import url
+import re
 
-def noti_page_parser(noti_link, code_link):
+
+#크롤링 결과물에 광고를 추가하는 함수
+def puting_ad():
+
+    #TODO
+    None
+
+def noti_page_parser(noti_link):
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(noti_link, headers=headers)
+    data = requests.get(noti_link + 'list#none', headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    #리턴용 리스트 및 변수 작성
-    Noti_Compression = list()
+    noti_dic = {'link': noti_link + 'detail/'}
 
-    for i in range(1, 10):
+    #리턴용 리스트 및 변수 작성
+    Noti_Compression = [['일반 공지', noti_link + 'detail/']]
+
+
+    #한 페이지 전체를 크롤링 하도록 16 으로 설정
+    for i in range(1, 16):
         Noti_num_list = list()
 
-        # 공지 제목
-        title = soup.select_one(
-            '#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.top > p' % i)
-        Noti_num_list.append(title.get_text(strip=True))
 
-        # 고유 코드
-        title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.mark > span' % i)
-        Noti_num_list.append(code_link + title.get_text(strip=True))
-
-        if (title.get_text(strip=True) == "공지"):
+        #공지인 경우 크롤링을 진행하지 않음
+        title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.top > p > span' % i)
+        if title is not None:
             continue
 
-        #공지 날짜
+        # 공지 제목 추출
+        title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.top > p' % i)
+        Notice_Title = title.get_text(strip=True)
+
+        '''
+        #공지 날짜 추출
         title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a > div.top > div.info > span:nth-child(1)' %i)
         Noti_num_list.append(title.get_text(strip=True))
+        '''
 
+
+        # 링크 고유 코드 추출
+        title = soup.select_one('#wrap > div.ly-right > div.contents > div > div.board > div.board_list > ul > li:nth-child(%d) > a' %i)
+        onclick_value = title['onclick']
+        match = re.search(r'goDetail\((\d+)\)', onclick_value)
+
+        #리스트 저장 부분
+        Noti_num_list.append(Notice_Title)
+        Noti_num_list.append(match.group(1))
         Noti_Compression.append(Noti_num_list)
 
+
+        #딕셔너리 저장 부분
+        noti_dic[match.group(1)] = Notice_Title
+
+
+        #공지 내용이 5개라면 크롤링 중지?
+        if(len(noti_dic) == 6):
+            break
+
+    puting_ad()
+    #TODO
+
     return Noti_Compression
+
+
+print(noti_page_parser(url.general))
+#print(noti_page_parser(url.empprg))
+#noti_page_parser('https://wise.dongguk.ac.kr/article/acdnotice/')
